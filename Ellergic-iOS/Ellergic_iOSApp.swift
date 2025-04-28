39ecda0da7d3cddd24b1a6c462ad7a1ad91d5b68
Derek Howes <d.howes24@gmail.com>
2025-04-28 14:45:44 -0400

    Initial Commit

---
+++ Ellergic-iOS/Ellergic_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ellergic_iOSApp.swift
+//  Ellergic-iOS
+//
+//  Created by Derek Howes on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Ellergic_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
