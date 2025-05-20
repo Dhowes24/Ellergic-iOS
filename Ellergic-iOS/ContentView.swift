e10f0b447f7272ef75aaa8c60ac1fde8febe0a79
Derek Howes <d.howes24@gmail.com>
2025-05-20 15:59:38 -0400

    initial navigation commit

--- Ellergic-iOS/ContentView.swift
+++ Ellergic-iOS/ContentView.swift
@@ -4,17 +4,37 @@ import SwiftUI
 import Foundation
 
 struct ContentView: View {
+    @State private var path = NavigationPath()
     @StateObject private var docViewModel = DocumentScannerViewModel()
+    @State private var offset = CGSize.zero
+
+    var drag: some Gesture {
+        DragGesture(minimumDistance: 50)
+            .onChanged { value in
+                if value.translation.width < 0 {
+                    offset = CGSize(width: value.translation.width, height: 0)
+                }
+            }
+            .onEnded { value in
+                if value.translation.width < -50 { path.append(10) }
+                offset = .zero
+            }
+    }
 
     var body: some View {
-        ZStack {
-            DocumentScannerView(viewModel: docViewModel)
-        }
-        .ignoresSafeArea()
-        .sheet(isPresented: $docViewModel.showModal) {
-            VStack {
-                if let returnedResults = docViewModel.returnedResults {
-                    Text(returnedResults.foods[0].foodName)
+        NavigationStack(path: $path) {
+            ZStack {
+                DocumentScannerView(viewModel: docViewModel)
+            }
+            .offset(offset)
+            .gesture(drag)
+
+            .ignoresSafeArea()
+            .sheet(isPresented: $docViewModel.showModal) {
+                VStack {
+                    if let returnedResults = docViewModel.returnedResults {
+                        Text(returnedResults.foods[0].foodName)
+                    }
                 }
             }
         }
